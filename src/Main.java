4f82941dee599542be2467007ee78c65e986681e
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-01-30 10:10:10 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
             Person p1 = new Person();
         System.out.println("Welcome to Address Book");
         Addressbookservice a1 = new Addressbookservice();
-
+        a1.setval(p1);
+        System.out.println(a1.display(p1));
         Addressbook b1 = new Addressbook();
         b1.contactList.put(1,p1);
         System.out.println(b1.contactList);
