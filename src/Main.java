022a4aef58185bf9ba84e65eb54a604e7542f5fe
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-02 10:51:53 +0530

    Add initial files

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-            Person p1 = new Person();
         System.out.println("Welcome to Address Book");
+        Person p1 = new Person();
         Addressbookservice a1 = new Addressbookservice();
-        a1.setval(p1);
-        System.out.println(a1.display(p1));
+        Scanner scan = new Scanner(System.in);
+        int mulContacts = scan.nextInt();
+        for(int i=0;i<mulContacts;i++) {
+            a1.setval(p1);
+            System.out.println(a1.display(p1));
+        }
         Addressbook b1 = new Addressbook();
         b1.contactList.put(1,p1);
         b1.addPerson(1,p1);
-        Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter person's first name");
         String deleteFname = scan.next();
         System.out.println("Enter person's last name");
